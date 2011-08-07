b998085df2588032de48376da89c453856d08957
Michael W <gcr@sneakygcr.net>
2011-08-07 10:45:11 -0600

    Properly implemented hooks this time

---
+++ plugins/hilight.js
@@ -0,0 +1,16 @@
+var hilightColor=require('../lib/display_administrivia').white;
+var HILIGHTS=[
+    /gcr/gi,
+    /blank/gi,
+];
+
+exports.init = function(chat, client) {
+  client.addDisplayHook(function(msg,cc) {
+                          for (i=0,l=HILIGHTS.length; i<l; i++) {
+                            msg = msg.replace(HILIGHTS[i],
+                                              hilightColor("$&"));
+                          }
+                          cc(msg);
+                        });
+
+};
