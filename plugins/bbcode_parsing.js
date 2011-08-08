db57d6665c0be5a187031b1afa3b4010dc8071e9
Michael W <gcr@sneakygcr.net>
2011-08-07 22:58:39 -0600

    Added  "reverse bbcode" to allow you to have *this* turn into [b]this[/b]

--- plugins/bbcode_parsing.js
+++ plugins/bbcode_parsing.js
@@ -5,6 +5,7 @@ var italic = term_code("1");
 var bold = term_code("1");
 
 exports.init = function(chat,client) {
+
   client.addDisplayHook(
     function(text, cc) {
       // Pulled straight form official_client.js
@@ -17,4 +18,24 @@ exports.init = function(chat,client) {
 
       cc(text);
     });
+
+  var verbatim=false;
+
+  client.addMessageHook(
+    function(msg, cc){
+      // Now do the reverse, converting *this* into [b]this[/b].
+      if (verbatim) { return cc(msg); }
+      cc(msg
+         .replace(/\*(.+?)\*/gi, "[b]$1[/b]")
+         .replace(/\/(.+?)\//gi, "[i]$1[/i]")
+         .replace(/\_(.+?)\_/gi, "[u]$1[/u]")
+      );
+    });
+
+  client.addCommand("verbatim",
+                    function() {
+                      verbatim = !verbatim;
+                      client.display.debug("Message sending: "+
+                                           (verbatim?"verbatim":"converted to bbcode"));
+                    });
 };
